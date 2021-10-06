a03614b11382e449012f7895d602c04ca8b8b2dc
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-10-06 17:16:45 -0400

    changes

--- test.js
+++ test.js
@@ -1,16 +1,13 @@
 console.log ('hi there');
 
-document.addEventListener ('DOMContentLoaded', function (event) {
-  const grabDiv = document.querySelector ('#orb');
+const grabDiv = document.getElementById ('orb');
 
-  grabDiv.addEventListener ('mouseover', () => {
-    grabDiv.classList.add ('orb');
-    console.log ('mousing over!');
-  });
+grabDiv.addEventListener ('mouseover', () => {
+  grabDiv.classList.add ('orb');
+  console.log ('mousing over!');
+});
 
-  grabDiv.addEventListener ('mouseout', () => {
-    grabDiv.classList.remove ('orb');
-    console.log ('mousing out');
-  });
-  console.log (event);
+grabDiv.addEventListener ('mouseout', () => {
+  grabDiv.classList.remove ('orb');
+  console.log ('mousing out');
 });
