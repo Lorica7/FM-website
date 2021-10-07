3152b3d43abbb459da7461e6487bd76140ff87a3
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-10-07 08:47:11 -0400

    orb2

--- test.js
+++ test.js
@@ -11,3 +11,15 @@ grabDiv.addEventListener ('mouseout', () => {
   grabDiv.classList.remove ('orb');
   console.log ('mousing out');
 });
+
+const grabDiv2 = document.getElementById ('orb2');
+
+grabDiv2.addEventListener ('mouseover', () => {
+  grabDiv.classList.add ('orb');
+  console.log ('mousing over!');
+});
+
+grabDiv2.addEventListener ('mouseout', () => {
+  grabDiv.classList.remove ('orb');
+  console.log ('mousing out');
+});
