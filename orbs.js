f6df6fd17778eecce353becd70f2a05daffc006c
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-10-11 11:49:15 -0400

    changed var names

--- orbs.js
+++ orbs.js
@@ -1,66 +1,67 @@
 /* ****** Product Vendor Services *****************************/
-const grabDiv = document.getElementById ('pvs-border');
+const grabTarget = document.getElementById ('pvs-border');
 const grabCircle = document.getElementById ('circle');
 
-grabDiv.addEventListener ('mouseover', () => {
+grabTarget.addEventListener ('mouseover', () => {
   grabCircle.classList.add ('orb');
 });
 
-grabDiv.addEventListener ('mouseout', () => {
+grabTarget.addEventListener ('mouseout', () => {
   grabCircle.classList.remove ('orb');
 });
 
 /***************Two 22222222222  Real Estate Group   */
+const grabTarget2 = document.getElementById ('target');
 
-const grabDiv2 = document.getElementById ('circle2');
-const getTarget = document.getElementById ('target');
+const grabCircle2 = document.getElementById ('circle2');
 
-getTarget.addEventListener ('mouseover', () => {
-  grabDiv2.classList.add ('orb');
+grabTarget2.addEventListener ('mouseover', () => {
+  grabCircle2.classList.add ('orb');
 });
 
-getTarget.addEventListener ('mouseout', () => {
-  grabDiv2.classList.remove ('orb');
+grabTarget2.addEventListener ('mouseout', () => {
+  grabCircle2.classList.remove ('orb');
 });
 
 /*  THREE 3333333333333333333333333333 **************/
 
-const getTarget3 = document.getElementById ('orb3');
-const getThree = document.getElementById ('circle-three');
+const getTarget3 = document.getElementById ('target-three');
+const getCircle3 = document.getElementById ('circle-three');
 
 getTarget3.addEventListener ('mouseover', () => {
-  getThree.classList.add ('orb');
-  console.log ('three here');
+  getCircle3.classList.add ('orb');
 });
 
 getTarget3.addEventListener ('mouseout', () => {
-  getThree.classList.remove ('orb');
+  getCircle3.classList.remove ('orb');
 });
 
 /* ************************ Four ( R&D) **********************/
 
 const getTarget4 = document.getElementById ('target-four');
-const orbHolder = document.getElementById ('orb-holder');
+const getCircle4 = document.getElementById ('circle-four');
 
 getTarget4.addEventListener ('mouseover', () => {
-  orbHolder.classList.add ('orb');
+  getCircle4.classList.add ('orb');
   console.log ('Four here');
 });
 
 getTarget4.addEventListener ('mouseout', () => {
-  orbHolder.classList.remove ('orb');
+  getCircle4.classList.remove ('orb');
 });
 
 /**************FIVE******************** */
 
 const getTarget5 = document.getElementById ('target-five');
-const orbHolder2 = document.getElementById ('orb-holder-two');
+const getCircle5 = document.getElementById ('circle-five');
 
 getTarget5.addEventListener ('mouseover', () => {
-  orbHolder2.classList.add ('orb');
+  getCircle5.classList.add ('orb');
   console.log ('Four here');
 });
 
 getTarget5.addEventListener ('mouseout', () => {
-  orbHolder2.classList.remove ('orb');
+  getCircle5.classList.remove ('orb');
 });
+
+const pvg = document.querySelectorAll ('purple-back');
