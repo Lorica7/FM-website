9f7867186281019859438435bed6e00057b3eba6
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-10-22 15:29:24 -0400

    altered classChanger

--- newletter.js
+++ newletter.js
@@ -1,12 +1,14 @@
 window.onscroll = function (ev) {
-  console.log (ev);
-  console.log.log ('no way!!!');
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
     setTimeout (classChanger, 5000);
+    console.log (ev);
+    console.log ('no way!!!');
   }
 };
 
 let classChanger = function () {
+  console.log ('no way!!!');
+
   document.getElementById ('banner1').classList.remove ('closed');
   document.getElementById ('banner1').classList.add ('slide-up');
 };
