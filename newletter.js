af8f0c3e5d4a7654f309baad589ee3602c510f12
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-10-22 15:21:16 -0400

    trying again

--- newletter.js
+++ newletter.js
@@ -1,6 +1,12 @@
 window.onscroll = function (ev) {
+  console.log (ev);
+  console.log.log ('no way!!!');
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-    document.getElementById ('banner1').classList.remove ('closed');
-    document.getElementById ('banner1').classList.add ('slide-up');
+    setTimeout (classChanger, 5000);
   }
+};
+
+let classChanger = function () {
+  document.getElementById ('banner1').classList.remove ('closed');
+  document.getElementById ('banner1').classList.add ('slide-up');
 };
