dcd28d58e06824c0747dd018ba9fe9de5f8c13c2
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-10-22 16:27:07 -0400

    changed classes

--- newletter.js
+++ newletter.js
@@ -1,6 +1,6 @@
 window.onscroll = function (ev) {
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-    setTimeout (classChanger, 5000);
+    setTimeout (classChanger, 2500);
     console.log (ev);
     console.log ('no way!!!');
   }
@@ -8,7 +8,5 @@ window.onscroll = function (ev) {
 
 let classChanger = function () {
   console.log ('no way!!!');
-
-  document.getElementById ('banner1').classList.remove ('closed');
-  document.getElementById ('banner1').classList.add ('slide-up');
+  document.getElementById ('banner1').classList.add ('moved');
 };
