3cb589601702e93c865c3f8778b662de4e4559a3
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-11-11 11:00:29 -0500

    changed banner code

--- email.js
+++ email.js
@@ -1,14 +1,10 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-javascript;
+
 const sgMail = require ('@sendgrid/mail');
 sgMail.setApiKey (process.env.SENDGRID_API_KEY);
 const msg = {
-  to: 'hello@daniellelamorte.com', // Change to your recipient
-  from: 'test@example.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  email: 'hello@daniellelamorte.com', // Change to your recipient
 };
 sgMail
   .send (msg)
--- newletter.js
+++ newletter.js
@@ -2,10 +2,20 @@ window.onscroll = function (ev) {
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
     setTimeout (classChanger, 1500);
     console.log (ev);
-    console.log ('no way!!!');
   }
 };
 
 let classChanger = function () {
   document.getElementById ('banner1').classList.add ('moved');
+  document.getElementById ('wrapped-frame').classList.add ('moved');
 };
+
+// let API_url = 'https://api.sendgrid.com/v3/';
+
+// let reqBody = {
+//   contacts: [
+//     {
+//       email: 'string (required)',
+//     },
+//   ],
+// };
